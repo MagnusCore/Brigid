e1bcaaeef246e5534e6b9c2de7d14f011e4d3261
John Purtle <purtlejohnmark@gmail.com>
2018-12-05 15:26:38 -0600

    Update examples/basic/autoload.php

--- examples/basic/autoload.php
+++ examples/basic/autoload.php
@@ -4,10 +4,13 @@ namespace MagnusApp {
 
 		$className = str_replace("\\", '/', $className);
 		$vendors   = dirname(__DIR__) . '/vendor/';
+		$appDir   = __DIR__ . '/app/';
 		$sourceDir = dirname(dirname(dirname(__DIR__)));
 
 		if (file_exists($vendors . $className . '.php')) {
 			require_once $vendors . $className . '.php';
+		} elseif (file_exists($appDir . $className . '.php')) {
+			require_once $appDir . $className . '.php';	
 		} else if (file_exists(__DIR__ . '/' . $className . '.php')) {
 			require_once __DIR__ . '/' . $className . '.php';
 		} else if (file_exists(__DIR__ . '/vendor/' . $className . '.php')) {
